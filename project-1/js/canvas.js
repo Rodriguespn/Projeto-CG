69f6f047aaa6ffec893da25064edde43715b9d89
Pedro Rodrigues <44656907+Rodriguespn@users.noreply.github.com>
2020-10-03 13:19:28 +0100

    mobile draw

---
+++ project-1/js/canvas.js
@@ -0,0 +1,219 @@
+let windowWidth = window.innerWidth
+let windowHeight = window.innerHeight
+let scene, camera, renderer, geometry, material, mesh, ball
+
+// inital coordinates of the camera
+const initCameraPosition = {
+    cameraX: 15, 
+    cameraY: 10, 
+    cameraZ: 30
+}
+
+// the default color of the diferente objects
+const objectsColors = {
+    wire: 0x5e5e5e,
+    cube: 0xee1122,
+    circle: 0x1122ee,
+    ellipse: 0x4edf22
+}
+
+// draws the object on the canvas
+function render() {
+    renderer.render(scene, camera)
+}
+
+// creates a cube at the (x,y,z) position
+function createCube(obj, x, y, z) {
+    geometry = new THREE.CubeGeometry(2, 0.1, 1)
+    material = new THREE.MeshBasicMaterial({ color: objectsColors.cube, wireframe: true })
+    mesh = new THREE.Mesh(geometry, material)
+
+    mesh.position.set(x, y, z)
+
+    obj.add(mesh)
+}
+
+// creates a ellipse at the (x,y,z) position
+function createEllipse(obj, x, y, z) {
+    const path = new THREE.Shape();
+    path.absellipse(0,0,0.5,1,0, Math.PI*2, false,0);
+    geometry = new THREE.ShapeBufferGeometry(path);
+
+    material = new THREE.MeshBasicMaterial({ color: objectsColors.ellipse, side: THREE.DoubleSide, wireframe: true })
+    mesh = new THREE.Mesh(geometry, material)
+
+    mesh.rotation.x = Math.PI/2
+    mesh.rotation.z = Math.PI/2
+    mesh.position.set(x, y, z)
+
+    obj.add(mesh)
+    
+}
+
+// creates a circle at the (x,y,z) position
+function createCircle(obj, x, y, z) {
+    geometry = new THREE.CylinderGeometry(1, 1, 0.1);
+
+    material = new THREE.MeshBasicMaterial({ color: objectsColors.circle, wireframe: true })
+    mesh = new THREE.Mesh(geometry, material)
+
+    mesh.position.set(x, y, z)
+
+    obj.add(mesh)
+}
+
+// creates a vertical cable at the (x,y,z) position
+function createVerticalWire(obj, heigth, x, y, z) {
+    geometry = new THREE.CylinderGeometry(0.1, 0.1, heigth, 8)
+
+    material = new THREE.MeshBasicMaterial({ color: objectsColors.wire, wireframe: true })
+    mesh = new THREE.Mesh(geometry, material)
+
+    mesh.position.set(x, y, z)
+
+    obj.add(mesh)
+}
+
+// creates a horizontal cable at the (x,y,z) position
+function createHorizontalWire(obj, height, x, y, z) {
+    geometry = new THREE.CylinderGeometry(0.1, 0.1, height, 8)
+    material = new THREE.MeshBasicMaterial({ color: objectsColors.wire, wireframe: true })
+    mesh = new THREE.Mesh(geometry, material)
+
+    mesh.rotation.z = Math.PI / 2
+    mesh.position.set(x, y, z)
+
+    obj.add(mesh)
+}
+
+// creates the mobile
+function createMobile(x, y, z) {
+    const mobile = new THREE.Object3D()
+
+    // Vertical wires
+    createVerticalWire(mobile, 4, 0, 2, 0)
+    createVerticalWire(mobile, 2, 3, -1, 0)
+    createVerticalWire(mobile, 2, 6, -3, 0)
+    createVerticalWire(mobile, 2, 9, -5, 0)
+    createVerticalWire(mobile, 2, 12, -7, 0)
+
+    // Horizontal wires
+    createHorizontalWire(mobile, 10, 0, 0, 0)
+    createHorizontalWire(mobile, 10, 3, -2, 0)
+    createHorizontalWire(mobile, 10, 6, -4, 0)
+    createHorizontalWire(mobile, 10, 9, -6, 0)
+    createHorizontalWire(mobile, 10, 12, -8, 0)
+
+    // Cubes
+    createCube(mobile, -6, 0, 0)
+    createCube(mobile, 9, -2, 0)
+    createCube(mobile, 3, -6, 0)
+    createCube(mobile, 6, -8, 0)
+    createCube(mobile, 18, -8, 0)
+
+    // Cylinders
+    createEllipse(mobile, 6, 0, 0)
+    createCircle(mobile, -3, -2, 0)
+    createCircle(mobile, 0, -4, 0)
+    createEllipse(mobile, 12, -4, 0)
+    createCircle(mobile, 15, -6, 0)
+
+    scene.add(mobile)
+
+    mobile.position.x = x
+    mobile.position.y = y
+    mobile.position.z = z
+}
+
+// updates the position of the orthogonal camera
+function updateCameraPosition(x, y, z) {
+    camera.position.x = x
+    camera.position.y = y
+    camera.position.z = z
+    camera.lookAt(scene.position)
+}
+
+// creates the camera object
+function createCamera() {
+    camera = new THREE.PerspectiveCamera(70, windowWidth / windowHeight, 1, 1000)
+
+    updateCameraPosition(initCameraPosition.cameraX, initCameraPosition.cameraY, initCameraPosition.cameraZ)
+}
+
+// creates the scene object
+function createScene() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xd5e3e3);
+
+    scene.add(new THREE.AxesHelper(10))
+
+    createMobile(0,0,0)
+}
+
+// adjusts the camera position when the window is resized
+function onResize() {
+    windowWidth = window.innerWidth
+    windowHeight = window.innerHeight
+
+    renderer.setSize(windowWidth, windowHeight)
+
+    if (windowHeight > 0 && windowWidth > 0) {
+        camera.aspect = renderer.getSize().width / renderer.getSize().height
+        camera.updateProjectionMatrix()
+    }
+}
+
+// handles an event when a key is pressed
+function onKeyDown(event) {
+    console.log('key pressed: ' + event.key)
+    switch (event.key) {
+        case '1':
+            updateCameraPosition(initCameraPosition.cameraX, initCameraPosition.cameraY, initCameraPosition.cameraZ)
+            break;
+
+        case '2':
+            updateCameraPosition(0, 30, 0)
+            break;
+
+        case '3':
+            updateCameraPosition(0, 0, 30)
+            break;
+
+        case '4':
+            scene.traverse((node) => {
+                if (node instanceof THREE.Mesh) {
+                    node.material.wireframe = !node.material.wireframe
+                }
+            })
+            break;
+
+        default:
+            break;
+    }
+}
+
+// animates the scene
+function animate() {
+    render()
+
+    requestAnimationFrame(animate)
+}
+
+// initial draw of the scene
+function init() {
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+
+    renderer.setSize(windowWidth, windowHeight)
+
+    document.body.appendChild(renderer.domElement)
+
+    createScene()
+    createCamera()
+
+    render()
+
+    window.addEventListener("resize", onResize)
+    window.addEventListener('keydown', onKeyDown)
+}
+
+
