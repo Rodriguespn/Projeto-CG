59df3987a52f0eae97bbef809526194a8389d1ac
HackerLorenzo <vicente.aser.lorenzo@gmail.com>
2020-10-06 16:17:17 +0100

    Minor Fixes

--- project-1/js/canvas.js
+++ project-1/js/canvas.js
@@ -23,18 +23,18 @@ function render() {
 }
 
 // creates a cube at the (x,y,z) position
-function createCube(obj, x, y, z) {
+function createCube(x, y, z) {
     geometry = new THREE.CubeGeometry(2, 0.1, 1)
     material = new THREE.MeshBasicMaterial({ color: objectsColors.cube, wireframe: true })
     mesh = new THREE.Mesh(geometry, material)
 
     mesh.position.set(x, y, z)
 
-    obj.add(mesh)
+    return mesh
 }
 
 // creates a ellipse at the (x,y,z) position
-function createEllipse(obj, x, y, z) {
+function createEllipse(x, y, z) {
     const path = new THREE.Shape();
     path.absellipse(0,0,0.5,1,0, Math.PI*2, false,0);
     geometry = new THREE.ShapeBufferGeometry(path);
@@ -46,12 +46,11 @@ function createEllipse(obj, x, y, z) {
     mesh.rotation.z = Math.PI/2
     mesh.position.set(x, y, z)
 
-    obj.add(mesh)
-    
+    return mesh
 }
 
 // creates a circle at the (x,y,z) position
-function createCircle(obj, x, y, z) {
+function createCircle(x, y, z) {
     geometry = new THREE.CylinderGeometry(1, 1, 0.1);
 
     material = new THREE.MeshBasicMaterial({ color: objectsColors.circle, wireframe: true })
@@ -59,11 +58,11 @@ function createCircle(obj, x, y, z) {
 
     mesh.position.set(x, y, z)
 
-    obj.add(mesh)
+    return mesh
 }
 
 // creates a vertical cable at the (x,y,z) position
-function createVerticalWire(obj, heigth, x, y, z) {
+function createVerticalWire(heigth, x, y, z) {
     geometry = new THREE.CylinderGeometry(0.05, 0.05, heigth, 8)
 
     material = new THREE.MeshBasicMaterial({ color: objectsColors.wire, wireframe: true })
@@ -71,11 +70,11 @@ function createVerticalWire(obj, heigth, x, y, z) {
 
     mesh.position.set(x, y, z)
 
-    obj.add(mesh)
+    return mesh
 }
 
 // creates a horizontal cable at the (x,y,z) position
-function createHorizontalWire(obj, height, x, y, z) {
+function createHorizontalWire(height, x, y, z) {
     userData = { rotation: false, speed: 0 }
     
     geometry = new THREE.CylinderGeometry(0.05, 0.05, height, 8)
@@ -85,85 +84,39 @@ function createHorizontalWire(obj, height, x, y, z) {
     mesh.rotation.z = Math.PI / 2
     mesh.position.set(x, y, z)
 
-    obj.add(mesh)
+    return mesh
 }
 
-function createFirstMobileSegment(mobile) {
+function createMobileSegment(vertWire, horWire, leftObj, rightObj, name) {
     segment = new THREE.Object3D()
 
-    createHorizontalWire(segment, 10, 0, 0, 0)
-    createCube(segment, -6, 0, 0)
-    createEllipse(segment, 6, 0, 0)
+    segment.add(vertWire)
+    segment.add(horWire)
+    segment.add(leftObj)
+    segment.add(rightObj)
 
-    segment.name = "segment"
+    segment.name = name
 
-    mobile.add(segment)
-}
-
-function createSecondMobileSegment(mobile) {
-    segment = new THREE.Object3D()
-
-    createHorizontalWire(segment, 10, 3, -2, 0)
-    createCube(segment, 9, -2, 0)
-    createCircle(segment, -3, -2, 0)
-
-    segment.name = "segment"
-
-    mobile.add(segment)
-}
-
-function createThirdMobileSegment(mobile) {
-    segment = new THREE.Object3D()
-
-    createHorizontalWire(segment, 10, 6, -4, 0)
-    createCircle(segment, 0, -4, 0)
-    createEllipse(segment, 12, -4, 0)
-
-    segment.name = "segment"
-
-    mobile.add(segment)
-}
-
-function createFourthMobileSegment(mobile) {
-    segment = new THREE.Object3D()
-
-    createHorizontalWire(segment, 10, 9, -6, 0)
-    createCube(segment, 3, -6, 0)
-    createCircle(segment, 15, -6, 0)
-
-    segment.name = "segment"
-
-    mobile.add(segment)
-}
-
-function createFifthMobileSegment(mobile) {
-    segment = new THREE.Object3D()
- 
-    createHorizontalWire(segment, 10, 12, -8, 0)
-    createCube(segment, 6, -8, 0)
-    createCube(segment, 18, -8, 0)
-
-    segment.name = "segment"
-
-    mobile.add(segment)
+    return segment
 }
 
 // creates the mobile
 function createMobile(x, y, z) {
     const mobile = new THREE.Object3D()
 
-    // Vertical wires
-    createVerticalWire(mobile, 4, 0, 2, 0)
-    createVerticalWire(mobile, 2, 3, -1, 0)
-    createVerticalWire(mobile, 2, 6, -3, 0)
-    createVerticalWire(mobile, 2, 9, -5, 0)
-    createVerticalWire(mobile, 2, 12, -7, 0)
+    // Create Segments
+    const seg1 = createMobileSegment(createVerticalWire(4, 0, 2, 0), createHorizontalWire(10, 0, 0, 0), createCube(-6, 0, 0), createEllipse(6, 0, 0), "1stWire")
+    const seg2 = createMobileSegment(createVerticalWire(2, 3, -1, 0), createHorizontalWire(10, 3, -2, 0), createCube(9, -2, 0), createCircle(-3, -2, 0),  "2ndWire")
+    const seg3 = createMobileSegment(createVerticalWire(2, 6, -3, 0), createHorizontalWire(10, 6, -4, 0), createCircle(0, -4, 0), createEllipse(12, -4, 0),  "3rdWire")
+    const seg4 = createMobileSegment(createVerticalWire(2, 9, -5, 0), createHorizontalWire(10, 9, -6, 0), createCube(3, -6, 0), createCircle(15, -6, 0),  "4thWire")
+    const seg5 = createMobileSegment(createVerticalWire(2, 12, -7, 0), createHorizontalWire(10, 12, -8, 0), createCube(6, -8, 0), createCube(18, -8, 0),  "5thWire")
 
-    createFirstMobileSegment(mobile)
-    createSecondMobileSegment(mobile)
-    createThirdMobileSegment(mobile)
-    createFourthMobileSegment(mobile)
-    createFifthMobileSegment(mobile)
+    // Object Nesting
+    mobile.add(seg1)
+    seg1.add(seg2)
+    seg2.add(seg3)
+    seg3.add(seg4)
+    seg4.add(seg5)
 
     scene.add(mobile)
 
